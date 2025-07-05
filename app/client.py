bcfe88d5c1cc54ee783d0daeab9b85f90476d74b
adil <adilsaid64@gmail.com>
2025-07-05 22:17:23 +0100

    style: bit cleanup

--- app/client.py
+++ app/client.py
@@ -17,10 +17,6 @@ async def main():
             #     "args": ["mathserver.py"],  # absolute path
             #     "transport": "stdio",
             # },
-            # "weather": {
-            #     "url": "http://mcp-weather-server:8000/mcp",
-            #     "transport": "streamable_http",
-            # },
             "todo": {
                 "url": "http://todo-server:8000/mcp",
                 "transport": "streamable_http",
@@ -35,25 +31,10 @@ async def main():
 
     agent = create_react_agent(model, tools)
 
-    # math_response = await agent.ainvoke(
-    #     {"messages": [{"role": "user", "content": "what's (3 + 5) x 12?"}]}
-    # )
-
-    # print("Math response:", math_response["messages"][-1].content)
-
-    # weather_response = await agent.ainvoke(
-    #     {
-    #         "messages": [
-    #             {"role": "user", "content": "what is the weather in California?"}
-    #         ]
-    #     }
-    # )
-    # print("Weather response:", weather_response["messages"][-1].content)
-
     todo_response = await agent.ainvoke(
         {"messages": [{"role": "user", "content": "What are my current todos?"}]}
     )
-    print("Weather response:", todo_response["messages"][-1].content)
+    print("Single-action response:", todo_response["messages"][-1].content)
 
     todo_response = await agent.ainvoke(
         {
